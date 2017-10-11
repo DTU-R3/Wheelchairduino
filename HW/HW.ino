7dc691db1d8ee97fe5d76888bbabc2fdc2f036fa
ZhongyuWang <zhongyuzone@gmail.com>
2017-10-11 11:16:32 +0200

    Arduino based demo to control the motors of the wheelchair

---
+++ HW/HW.ino
@@ -0,0 +1,113 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(9, 10); // RX, TX 
+
+#define FWD_L_PWM 5
+#define BACK_L_PWM 6
+#define FWD_R_PWM 23
+#define BACK_R_PWM 22
+
+void setup() {
+	
+	// open the serial port
+	Serial.begin(9600);
+  mySerial.begin(9600);
+ 
+	// Motion control
+	pinMode(FWD_L_PWM, OUTPUT);
+	pinMode(BACK_L_PWM, OUTPUT);
+	pinMode(FWD_R_PWM, OUTPUT);
+	pinMode(BACK_R_PWM, OUTPUT);
+	delay(1000);
+	
+	// Init all pins
+	analogWrite(FWD_L_PWM, 0);
+	analogWrite(BACK_L_PWM, 0);
+	analogWrite(FWD_R_PWM, 0);
+	analogWrite(BACK_R_PWM, 0);
+	delay(1000);
+}
+
+// Command init
+int len = 10;
+char cmd[10] = {'0', '0', '1', '0', '0', '0', '1', '0', '0', '0'};
+
+// Speed of the wheel
+int leftSpeed = 0;
+int rightSpeed = 0;
+
+void Park()
+{
+  analogWrite(FWD_L_PWM, 0);
+  analogWrite(BACK_L_PWM, 0);
+  analogWrite(FWD_R_PWM, 0);
+  analogWrite(BACK_R_PWM, 0);
+}
+
+int charToInt(char ch)
+{
+	int tmp = 0;
+	tmp = tmp * 10 + (ch - 48);
+	return tmp;
+}
+
+void loop() {
+	if (Serial.available() > 0) {
+		Serial.readBytes(cmd, len);
+	}
+ 
+  if (mySerial.available() > 0) {
+    mySerial.readBytes(cmd, len);
+    Serial.println(cmd);
+  }
+	
+	// Analyse command
+	if (cmd[0] == 'D' && cmd[1] == 'K') {
+
+		// calculate wheel speed
+		leftSpeed = charToInt(cmd[3]) * 100 + charToInt(cmd[4]) * 10 + charToInt(cmd[5]);
+		rightSpeed = charToInt(cmd[7]) * 100 + charToInt(cmd[8]) * 10 + charToInt(cmd[9]);
+		
+		switch (cmd[2]) {
+			// Backward
+			case '0':
+				analogWrite(FWD_L_PWM, 0);
+				analogWrite(BACK_L_PWM, leftSpeed);
+				break;
+			// Forward
+			case '2':			
+				analogWrite(FWD_L_PWM, leftSpeed);
+				analogWrite(BACK_L_PWM, 0);
+				break;   
+			// Park
+			default:
+				analogWrite(FWD_L_PWM, 0);
+				analogWrite(BACK_L_PWM, 0);
+				break;
+		}
+
+		switch (cmd[6]) {
+			// Backward
+			case '0':
+				analogWrite(FWD_R_PWM, 0);
+				analogWrite(BACK_R_PWM, rightSpeed);
+				break;
+			// Forward
+			case '2':
+				analogWrite(FWD_R_PWM, rightSpeed);
+				analogWrite(BACK_R_PWM, 0);
+				break;
+			// Park
+			default:
+				analogWrite(FWD_R_PWM, 0);
+				analogWrite(BACK_R_PWM, 0);
+				break;
+		}	
+	}
+	// Error command
+	else {
+		Serial.flush();
+    mySerial.flush();
+		Park();		
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # wheelchairduino
 Arduino-based wheelchair controller
+
+1. The program is used to control the electrical wheelchair or other mobile robot with similar configuration.
+2. The components used are a Teensy 3.2 board, a IBT-2 motor driver and a HM-10 Bluthtooth module.
+3. The micro-controller receive the command in the format 'DK 1 000 1 000'
+	First and second digits: header of the command
+	Third digit: Turning direction of the left wheel, 0-backward, 1-stop, 2-forward
+	Fourth to Sixth digits: Turning speed of the left wheel, maximum 255.
+	Seventh digit: Turning direction of the right wheel, 0-backward, 1-stop, 2-forward
+	Eighth to Tenth digits: Turning speed of the right wheel, maximum 255.
